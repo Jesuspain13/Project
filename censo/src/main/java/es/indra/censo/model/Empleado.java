aeb7d21a7bcbfc4b78175f37f4c62ef6ce2d662e
jjespana <jjespana@indra.es>
2019-07-31 09:27:02 +0200

    Complex and employee entities

--- censo/src/main/java/es/indra/censo/model/Empleado.java
+++ censo/src/main/java/es/indra/censo/model/Empleado.java
@@ -11,8 +11,8 @@ import javax.persistence.Table;
 @Table(name = "empleado")
 public class Empleado implements Serializable {
 
-	//Definición de los atributos de la tabla Empleado
-	
+	// Definición de los atributos de la tabla Empleado
+
 	@Id
 	@Column(name = "id_empleado")
 	private Integer idEmpleado;
@@ -23,79 +23,48 @@ public class Empleado implements Serializable {
 
 	@Column(name = "id_puesto")
 	private Integer idPuesto;
-	
-	
+
 	// Implementación de los Getters & Setters de la clase Empleado.
 
 	public Integer getIdEmpleado() {
 		return idEmpleado;
 	}
 
-
-
-
 	public void setIdEmpleado(Integer idEmpleado) {
 		this.idEmpleado = idEmpleado;
 	}
-
-
-
 
 	public String getNick() {
 		return nick;
 	}
 
-
-
-
 	public void setNick(String nick) {
 		this.nick = nick;
 	}
-
-
-
 
 	public String getNombre() {
 		return nombre;
 	}
 
-
-
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
-
-
 
 	public String getApellido() {
 		return apellido;
 	}
 
-
-
-
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-
-
-
 
 	public Integer getIdPuesto() {
 		return idPuesto;
 	}
 
-
-
-
 	public void setIdPuesto(Integer idPuesto) {
 		this.idPuesto = idPuesto;
 	}
-
-
-
 
 	private static final long serialVersionUID = 1L;
 
